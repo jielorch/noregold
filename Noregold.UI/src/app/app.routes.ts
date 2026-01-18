6e713125920a4309247f188ac6f6487a2fd77a9d
rain llorca <rainllorca@gmail.com>
2026-01-18 10:32:13 +0800

    updates

--- Noregold.UI/src/app/app.routes.ts
+++ Noregold.UI/src/app/app.routes.ts
@@ -4,5 +4,9 @@ export const routes: Routes = [
     {
         path:'',
         loadComponent: () => import('./features/pages/home/home').then(c => c.Home)
+    },
+    {
+        path:'inventory',
+        loadComponent:() => import('./features/pages/inventory/inventory').then(c => c.Inventory)
     }
 ];
